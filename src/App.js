c48cb08112c6a84d288e99666b08db76306bfa8a
Aaditya Semwal <93855836+aadityasemwal7@users.noreply.github.com>
2022-09-21 19:14:38 +0530

    Pushing complete product

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Spooky?type=single")
+      .then((res) => {
+        setData(res.data["joke"])
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+    <h1>Joke Generator</h1>
+    <h3>{data}</h3>
+    <footer>Copyright © Aaditya Semwal</footer>
+  </div>
   );
 }
 
